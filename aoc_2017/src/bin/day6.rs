9fc786dc30e4efc9d4742eb63c32c1ff339f493e
Shi Jian <timeflieslikeanarrow001@gmail.com>
2021-09-26 19:10:47 -0400

    check against the start_state instead

--- aoc_2017/src/bin/day6.rs
+++ aoc_2017/src/bin/day6.rs
@@ -4,19 +4,21 @@ use std::collections::HashSet;
 fn main() {
     let input = fs::read_to_string("day6.input").unwrap();
     let mut banks = parse(&input);
-    println!("part 1: {}", distribution_cycle(&mut banks));
-    println!("part 2: {}", distribution_cycle(&mut banks));
+    println!("part 1: {}", distribution_cycle(&mut banks, false));
+    println!("part 2: {}", distribution_cycle(&mut banks, true));
 }
 
 fn parse(input: &str) -> Vec<usize> {
     input.split_whitespace().map(|s| s.parse().unwrap()).collect()
 }
 
-fn distribution_cycle(banks: &mut Vec<usize>) -> usize {
+fn distribution_cycle(banks: &mut Vec<usize>, use_start_state: bool) -> usize {
     let n = banks.len();
     let mut cycles = 0;
     let mut states = HashSet::new();
     
+    let start_state = banks.clone();
+
     states.insert(banks.clone());
 
     loop {
@@ -38,8 +40,10 @@ fn distribution_cycle(banks: &mut Vec<usize>) -> usize {
         }
 
         let new_state = banks.clone();
-        if states.contains(&new_state) {
-            return cycles
+        if use_start_state && new_state == start_state {
+                return cycles;
+        } else if states.contains(&new_state) {
+                return cycles;
         }
 
         states.insert(new_state);
@@ -54,14 +58,14 @@ mod day6_tests {
     #[test]
     fn examples() {
         let mut banks = vec![0,2,7,0];
-        let cycle = distribution_cycle(&mut banks);
+        let cycle = distribution_cycle(&mut banks, false);
 
         //part 1
         assert_eq!(cycle, 5);
         assert_eq!(banks, vec![2,4,1,2]);
 
         //part 2
-        let cycle2 = distribution_cycle(&mut banks);
+        let cycle2 = distribution_cycle(&mut banks, true);
         assert_eq!(cycle2, 4);
     }
 }
