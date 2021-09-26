695fb668854eac86d2dd95013e2860186bbe3e4b
Shi Jian <timeflieslikeanarrow001@gmail.com>
2021-09-26 18:36:12 -0400

    day 5 part 2

--- aoc_2017/src/bin/day5.rs
+++ aoc_2017/src/bin/day5.rs
@@ -2,10 +2,12 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("day5.input").unwrap();
     let mut instructions = parse(&input);
-    println!("part 1: {}", escape_maze(&mut instructions));
+    let mut instructions2 = instructions.clone();
+    println!("part 1: {}", escape_maze(&mut instructions, increment_rule_part1));
+    println!("part 2: {}", escape_maze(&mut instructions2, increment_rule_part2));
 }
 
-fn escape_maze(instructions: &mut Vec<i32>) -> usize {
+fn escape_maze(instructions: &mut Vec<i32>, increment_rule: fn(i32)->i32) -> usize {
     let len = instructions.len() as i32;
     let mut ip = 0;
     let mut steps = 0;
@@ -15,7 +17,7 @@ fn escape_maze(instructions: &mut Vec<i32>) -> usize {
 
         let old_ip = ip as usize;
         ip += instructions[old_ip];
-        instructions[old_ip] += 1;
+        instructions[old_ip] += increment_rule(instructions[old_ip]);
     }
 
     steps
@@ -25,6 +27,19 @@ fn parse(input: &str) -> Vec<i32> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
+fn increment_rule_part1(_jump: i32) -> i32 {
+    1
+}
+
+fn increment_rule_part2(jump: i32) -> i32 {
+    if jump >= 3 {
+        -1
+    } else {
+        1
+    }
+}
+
+
 #[cfg(test)]
 mod day5_tests {
     use super::*;
@@ -32,8 +47,16 @@ mod day5_tests {
     #[test]
     fn part1_examples() {
        let mut instructions = vec![0, 3, 0, 1, -3];
-       let steps = escape_maze(&mut instructions);
+       let steps = escape_maze(&mut instructions, increment_rule_part1);
        assert_eq!(steps, 5);
        assert_eq!(instructions, vec![2,5,0,1,-2]);
+    }
+
+    #[test]
+    fn part2_examples() {
+       let mut instructions = vec![0, 3, 0, 1, -3];
+       let steps = escape_maze(&mut instructions, increment_rule_part2);
+       assert_eq!(steps, 10);
+       assert_eq!(instructions, vec![2, 3, 2, 3, -1]);
     }
 }
