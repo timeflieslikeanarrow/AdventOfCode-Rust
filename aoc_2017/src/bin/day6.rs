15777eef84785adf6781a772846968f477672770
Shi Jian <timeflieslikeanarrow001@gmail.com>
2021-09-26 19:05:02 -0400

    day 6 -- seems to work, but not entirely correct

---
+++ aoc_2017/src/bin/day6.rs
@@ -0,0 +1,67 @@
+use std::fs;
+use std::collections::HashSet;
+
+fn main() {
+    let input = fs::read_to_string("day6.input").unwrap();
+    let mut banks = parse(&input);
+    println!("part 1: {}", distribution_cycle(&mut banks));
+    println!("part 2: {}", distribution_cycle(&mut banks));
+}
+
+fn parse(input: &str) -> Vec<usize> {
+    input.split_whitespace().map(|s| s.parse().unwrap()).collect()
+}
+
+fn distribution_cycle(banks: &mut Vec<usize>) -> usize {
+    let n = banks.len();
+    let mut cycles = 0;
+    let mut states = HashSet::new();
+    
+    states.insert(banks.clone());
+
+    loop {
+        cycles += 1;
+
+        //find max
+        let mut max = *&banks[0];
+        let mut max_index = 0;
+        for i in 1..n {
+            if banks[i] > max {
+                max = banks[i];
+                max_index = i;
+            }
+        }
+
+        banks[max_index] = 0;
+        for i in max_index + 1 ..= max_index + max {
+            banks[i % n] += 1;
+        }
+
+        let new_state = banks.clone();
+        if states.contains(&new_state) {
+            return cycles
+        }
+
+        states.insert(new_state);
+    }
+
+}
+
+#[cfg(test)]
+mod day6_tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        let mut banks = vec![0,2,7,0];
+        let cycle = distribution_cycle(&mut banks);
+
+        //part 1
+        assert_eq!(cycle, 5);
+        assert_eq!(banks, vec![2,4,1,2]);
+
+        //part 2
+        let cycle2 = distribution_cycle(&mut banks);
+        assert_eq!(cycle2, 4);
+    }
+}
