06468515311b7ae60f9d915025bfa49107167284
Shi Jian <timeflieslikeanarrow001@gmail.com>
2021-09-27 01:32:08 -0400

    refactor Day 7

--- aoc_2017/src/bin/day7.rs
+++ aoc_2017/src/bin/day7.rs
@@ -18,39 +18,54 @@ fn main() {
 #[derive(Debug)]
 struct Program {
     name: String,
-    weight: usize,
-    children: Vec<String>
+    own_weight: usize,
+    children: Vec<String>,
+    total_weight: usize,
 }
 
 fn part1(input: &str) -> String {
-    let programs = parse(input);
+    let (programs, _) = parse(input);
     find_root(&programs)
 }
 
-fn parse_line(line: &str) -> Program {
+fn parse_line(line: &str, siblings: &mut HashMap<String, Vec<String>>) -> Program {
     let parts: Vec<_> = line.trim().split(" -> ").collect();
     let caps = PROGRAM_LINE.captures_iter(parts[0]).next().unwrap();
     let name = caps[1].to_owned();
-    let weight = *&caps[2].parse().unwrap();
+    let own_weight = *&caps[2].parse().unwrap();
     let mut children = Vec::new();
     if parts.len() > 1 {
         children = parts[1].trim().split(", ").map(|s| s.to_string()).collect();
     } 
 
+    add_siblings(&children, siblings);
+
     Program { 
         name, 
-        weight, 
-        children
+        own_weight, 
+        children,
+        total_weight: 0,
     }
 }
 
-fn parse(input: &str) -> HashMap<String, Program> {
-    HashMap::from_iter(
-        input.lines().map(|line| {
-            let p = parse_line(line);
-            (p.name.clone(), p)
-        })
-    )
+fn add_siblings(children: &Vec<String>, siblings: &mut HashMap<String, Vec<String>>) {
+    for child in children.iter() {
+        let s = children.iter()
+                    .filter(|child2| *child2 != child).map(|child2| child2.to_owned()).collect();
+
+        siblings.insert(child.to_string(), s);
+    }
+}
+
+fn parse(input: &str) -> (HashMap<String, Program>, HashMap<String, Vec<String>>) {
+    let mut siblings = HashMap::new();
+    let mut programs = HashMap::new();
+    for line in input.lines() {
+        let p = parse_line(line, &mut siblings);
+        programs.insert(p.name.clone(), p);
+    }
+
+    (programs, siblings)
 }
 
 fn find_root(programs: &HashMap<String, Program>) -> String {
@@ -64,17 +79,21 @@ fn find_root(programs: &HashMap<String, Program>) -> String {
 }
 
 fn part2(input: &str) -> usize {
-    let programs = parse(input);
+    let (mut programs, siblings) = parse(input);
+
     let weights: HashMap<String, usize> = programs.iter()
             .map(|(k, p)| (k.to_string(), compute_weights(p, &programs)))
             .collect();
-    let siblings: HashMap<String, Vec<String>> = compute_siblings(&programs);
 
-    let unbalanced = find_unbalanced(&programs, &weights);
-    let the_one = unbalanced.iter().filter(|p| children_balanced(p, &weights)).next().unwrap();
+    for (k, p) in programs.iter_mut() {
+        p.total_weight = weights[k];
+    }
+
+    let unbalanced = find_unbalanced(&programs);
+    let the_one = unbalanced.iter().filter(|p| children_balanced(p, &programs)).next().unwrap();
     let siblings = &siblings[&the_one.name];
 
-    the_one.weight - (weights[&the_one.name] - weights[&siblings[0]])
+    the_one.own_weight - (weights[&the_one.name] - weights[&siblings[0]])
 }
 
 fn compute_weights(parent: &Program, programs: &HashMap<String, Program>) -> usize {
@@ -83,48 +102,28 @@ fn compute_weights(parent: &Program, programs: &HashMap<String, Program>) -> usi
         compute_weights(child, programs)
     }).sum();
 
-    parent.weight + child_weights
+    parent.own_weight + child_weights
 }
 
-fn compute_siblings(programs: &HashMap<String, Program>) -> HashMap<String, Vec<String>> {
-    programs.values().flat_map(|p|
-        p.children.iter()
-            .map(move |c| 
-                (c.to_string(), 
-                 p.children.iter()
-                        .filter(|c2| *c2 != c)
-                        .map(|c2| c2.to_string()).collect()))
-    ).collect()
-}
-
-fn unbalanced_weights<'a>(children: &'a Vec<String>,  programs: &'a HashMap<String, Program>, weights: &HashMap<String, usize>) -> Vec<&'a Program> {
-    let mut children_weights = HashMap::new();
-
-    for child in children {
-        let weight = weights[child];
-        let entry = children_weights.entry(weight).or_insert(0);
-        *entry += 1;
-    }
-
-    let weight_set: HashSet<usize> = HashSet::from_iter(
-        children_weights.iter()
-            .filter(|(_, v)| **v == 1)
-            .map(|(k, _)| *k));
+fn unbalanced_programs<'a>(children: &'a Vec<String>,  programs: &'a HashMap<String, Program>) -> Vec<&'a Program> {
+    let children: Vec<_> = children.iter().map(|child| &programs[child]).collect();
 
     children.iter()
-        .filter(|child| weight_set.contains(&weights[*child]))
-        .map(|child| &programs[child])
+        .filter(|child| {
+            children.iter().filter(|child2| child.name != child2.name && child.total_weight == child2.total_weight).count() == 0
+        })
+        .map(|p| *p )
         .collect()
 }
 
-fn find_unbalanced<'a>(programs: &'a HashMap<String, Program>, weights: &'a HashMap<String, usize>) -> Vec<&'a Program> {
+fn find_unbalanced<'a>(programs: &'a HashMap<String, Program>) -> Vec<&'a Program> {
     programs.iter().flat_map(|(_, v)| {
-        unbalanced_weights(&v.children, programs, weights)
+        unbalanced_programs(&v.children, programs)
     }).collect()
 }
 
-fn children_balanced(program: &Program, weights: &HashMap<String, usize>) -> bool {
-    HashSet::<usize>::from_iter(program.children.iter().map(|child| weights[child])).len() == 1
+fn children_balanced(program: &Program, programs: &HashMap<String, Program>) -> bool {
+    HashSet::<usize>::from_iter(program.children.iter().map(|child| programs[child].total_weight)).len() == 1
 }
 
 #[cfg(test)]
