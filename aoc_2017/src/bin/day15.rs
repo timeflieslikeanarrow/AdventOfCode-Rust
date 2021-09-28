c3d2a509651e45d47f99fcc7afe67fa4fa4f775e
Shi Jian <timeflieslikeanarrow001@gmail.com>
2021-09-28 11:18:43 -0400

    day 15 part 2

--- aoc_2017/src/bin/day15.rs
+++ aoc_2017/src/bin/day15.rs
@@ -9,28 +9,49 @@ lazy_static! {
 
 fn main() {
     let input = fs::read_to_string("day15.input").unwrap();
-    println!("part 1: {}", part1(&input));
+    let captures = NUMBERS.captures(&input).unwrap();
+    let seed_a = captures[1].parse().unwrap();
+    let seed_b = captures[2].parse().unwrap();
+
+    println!("part 1: {}", count_matches(seed_a, 1, seed_b, 1, 40_000_000));
+    println!("part 2: {}", count_matches(seed_a, 4, seed_b, 8, 5_000_000));
 }
 
-fn part1(input: &str) -> usize {
-    let cpatures = NUMBERS.captures(input).unwrap();
-    let seed_a = cpatures[1].parse().unwrap();
-    let seed_b = cpatures[2].parse().unwrap();
-
-    //println!("{} {}",seed_a, seed_b);
-    count_matches(seed_a, seed_b, 40_000_000)
+struct Generator {
+    number: usize,
+    multiplier: usize,
+    divisor: usize
 }
 
-fn count_matches(seed_a: usize, seed_b: usize, pairs: usize) -> usize {
-    let m = 2147483647;
+impl Generator {
+    fn new(seed: usize, multiplier: usize, divisor: usize) -> Self {
+        Generator { number: seed, multiplier, divisor }
+    }
+}
+
+impl Iterator for Generator {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let new_number = self.number * self.multiplier % 2147483647;
+            self.number = new_number;
+            if new_number % self.divisor == 0 {
+                return Some(new_number);
+            }
+        }
+    }
+}
+
+fn count_matches(seed_a: usize, divisor_a: usize, seed_b: usize, divisor_b: usize, pairs: usize) -> usize {
     let bit_pattern = 0xFFFF;
-    let (multiplier_a, multiplier_b) = (16807, 48271);
-    let (mut a, mut b) = (seed_a, seed_b);
+
+    let mut generator_a = Generator::new(seed_a, 16807, divisor_a);
+    let mut generator_b = Generator::new(seed_b,  48271, divisor_b);
 
     let mut match_count = 0;
     for _ in 0..pairs {
-        a = a * multiplier_a % m;
-        b = b * multiplier_b % m;
+        let a = generator_a.next().unwrap();
+        let b = generator_b.next().unwrap();
 
         if a & & bit_pattern  == b & bit_pattern {
             match_count += 1;
@@ -46,6 +67,7 @@ mod day15_tests {
 
     #[test]
     fn part1_examples() {
-        assert_eq!(count_matches(65, 8921, 40_000_000), 588);
+        assert_eq!(count_matches(65, 1, 8921, 1, 40_000_000), 588);
+        assert_eq!(count_matches(65, 4, 8921, 8, 5_000_000), 309);
     }
 }
